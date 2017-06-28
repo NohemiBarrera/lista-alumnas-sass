c6f994ce23eee0c5fa9c5ad5170ad7ed61cdd7e1
Nohemi Barrera <nohemi.baol@gmail.com>
2017-06-27 21:18:18 -0500

    petición api

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
-var api = {
-  url: 'http://laboratoria.cuadra.co:9339/api/v1/students/'
-};
-
-var $listaAsistencia = $("#lista-asistencia");
-
 var cargarPagina = function(){
 	cargarAlumnas();
 
 };
 
-var cargarAlumnas = function(){
-	$.getJSON(api.id)
+var nuevaAlumna = "";
+var $listaAsistencia = $("#lista-asistencia");
+var plantilla = '<li>***nombre***<input type="checkbox"> </li>';
+
+var api = {
+  url: 'http://laboratoria.cuadra.co:9339/api/v1/students/'
 };
+
+var cargarAlumnas = function(){
+	$.getJSON(api.id, function(alumnas){
+		alumnas.forEach(mostrarAlumnas);
+	});
+};
+
+var mostrarAlumnas =  function(alumna){
+	var nombre = alumna.name;
+	var apellidoPaterno = alumna.pLastName;
+	var apellidoMaterno = alumna.mLastName;
+	nuevaAlumna = plantilla.replace("***nombre***", nombre + " " + apellidoPaterno + " " + apellidoMaterno + ".");
+	$listaAsistencia.append(nuevaAlumna);
+};
+
+$(document).ready(cargarPagina);
+
+
+
 
 
